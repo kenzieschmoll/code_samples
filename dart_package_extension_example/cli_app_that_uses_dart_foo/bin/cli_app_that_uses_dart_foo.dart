b80aa20e60b88ae8cc090f7dae77b0110a63135d
Kenzie Schmoll <kenzieschmoll@google.com>
2023-12-18 12:45:50 -0800

    Add dart_package_extension_example

---
+++ dart_package_extension_example/cli_app_that_uses_dart_foo/bin/cli_app_that_uses_dart_foo.dart
@@ -0,0 +1,68 @@
+import 'package:args/args.dart';
+import 'package:dart_foo/dart_foo.dart';
+
+const String version = '0.0.1';
+
+ArgParser buildParser() {
+  return ArgParser()
+    ..addFlag(
+      'help',
+      abbr: 'h',
+      negatable: false,
+      help: 'Print this usage information.',
+    )
+    ..addFlag(
+      'verbose',
+      abbr: 'v',
+      negatable: false,
+      help: 'Show additional command output.',
+    )
+    ..addFlag(
+      'version',
+      negatable: false,
+      help: 'Print the tool version.',
+    );
+}
+
+void printUsage(ArgParser argParser) {
+  print('Usage: dart cli_app_that_uses_dart_foo.dart <flags> [arguments]');
+  print(argParser.usage);
+}
+
+void main(List<String> arguments) async {
+  final ArgParser argParser = buildParser();
+  try {
+    final ArgResults results = argParser.parse(arguments);
+    bool verbose = false;
+
+    // Process the parsed arguments.
+    if (results.wasParsed('help')) {
+      printUsage(argParser);
+      return;
+    }
+    if (results.wasParsed('version')) {
+      print('cli_app_that_uses_dart_foo version: $version');
+      return;
+    }
+    if (results.wasParsed('verbose')) {
+      verbose = true;
+    }
+
+    // Act on the arguments provided.
+    print('Positional arguments: ${results.rest}');
+    if (verbose) {
+      print('[VERBOSE] All arguments: ${results.arguments}');
+    }
+  } on FormatException catch (e) {
+    // Print usage information if an invalid argument was provided.
+    print(e.message);
+    print('');
+    printUsage(argParser);
+  }
+
+  for (int i = 0; i < 100; i++) {
+    final awesome = Awesome().isAwesome;
+    print('am i awesome? $awesome');
+    await Future.delayed(const Duration(seconds: 10));
+  }
+}
